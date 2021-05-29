3900673be32a221c7eb5dbb1ff3936a3d2255868
Vital-jan <vitaljan@gmail.com>
2021-05-29 20:16:03 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+let persons = [];
+// let alphabetString = '';
+// for (let n = 65; n<= 90; n++) alphabetString += String.fromCharCode(n);
+// let alphabet = alphabetString.split('');
+
+class Main extends React.Component {
+    componentDidMount() {
+        axios.get(`https://yalantis-react-school-api.yalantis.com/api/task0/users`)
+            .then(res => {
+                Object.values(res.data).forEach((i)=>{
+                    persons.push(i);
+                })
+                console.log(persons)
+            })
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <Left />
+                <Right/>
+            </div>
+        )
+    }
+}
+const Letter = (props)=>{
+    return (
+        <div className="letter">
+            <div className="title">
+                {props.letter}
+            </div>
+        </div>
+    )
+}
+
+const Left = (props) => {
+    let letters = [];
+    for (let n = 65; n <= 90; n++) letters.push(<Letter letter={String.fromCharCode(n)} key={n}/>)
+    return (
+        <div className="left">
+            <div className="scroll">
+
+            {letters}
+            </div>
+        </div>
+    )
+}
+
+const Right = () => {
+    return (
+        <div className="right">
+        </div>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Main />
+    )
 }
 
 export default App;
